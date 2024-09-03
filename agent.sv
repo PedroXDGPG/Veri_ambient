be359e34ac2b6c100e7c2e0d95c1c2376d04ff18
pedroxdgpg <pedro.medinila@estudiantec.cr>
2024-09-03 10:40:30 -0600

    Correción agente

--- agent.sv
+++ agent.sv
@@ -1,53 +1,72 @@
-class ambiente #(parameter width = 16, parameter depth = 8);
-  // Declaración de los componentes del ambiente
-  driver #(.width(width)) driver_inst;
-  checker #(.width(width), .depth(depth)) checker_inst;
-  score_board #(.width(width)) scoreboard_inst;
-  agent #(.width(width), .depth(depth)) agent_inst;
+class agent #(parameter width = 16, parameter depth = 8);
+    trans_fifo_mbx agnt_drv_mbx; // Mailbox del agente al driver
+    comando_test_agent_mbx test_agent_mbx; // Mailbox del test al agente
+    int num_transacciones; // Número de transacciones para las funciones del agente
+    int max_retardo;
+    int ret_spec;
+    tipo_trans tpo_spec;
+    bit [width-1:0] dto_spec;
+    instrucciones_agente instruccion; // para guardar la última instruccion leída
+    trans_fifo #(.width(width)) transaccion;
 
-  // Definición de la interface que conecta el DUT
-  virtual fifo_if #(.width(width)) _if;
+    function new;
+        num_transacciones = 2;
+        max_retardo = 10;
+    endfunction
 
-  // declaración de los mailboxes
-  trans_fifo_mbx agnt_drv_mbx;         // mailbox del agente al driver
-  trans_fifo_mbx drv_chkr_mbx;         // mailbox del driver al checker
-  trans_sb_mbx chkr_sb_mbx;            // mailbox del checker al scoreboard
-  comando_test_sb_mbx test_sb_mbx;     // mailbox del test al scoreboard
-  comando_test_agent_mbx test_agent_mbx; // mailbox del test al agente
-
-  function new();
-    // Instanciación de los mailboxes
-    drv_chkr_mbx = new();
-    agnt_drv_mbx = new();
-    chkr_sb_mbx = new();
-    test_sb_mbx = new();
-    test_agent_mbx = new();
-
-    // Instanciación de los componentes del ambiente
-    driver_inst = new();
-    checker_inst = new();
-    scoreboard_inst = new();
-    agent_inst = new();
-
-    // Conexión de las interfaces y mailboxes en el ambiente
-    driver_inst.vif = _if;
-    driver_inst.drv_chkr_mbx = drv_chkr_mbx;
-    driver_inst.agnt_drv_mbx = agnt_drv_mbx;
-    checker_inst.drv_chkr_mbx = drv_chkr_mbx;
-    checker_inst.chkr_sb_mbx = chkr_sb_mbx;
-    scoreboard_inst.chkr_sb_mbx = chkr_sb_mbx;
-    scoreboard_inst.test_sb_mbx = test_sb_mbx;
-    agent_inst.test_agent_mbx = test_agent_mbx;
-    agent_inst.agnt_drv_mbx = agnt_drv_mbx;
-  endfunction
-
-  virtual task run();
-    $display("[%g] El ambiente fue inicializado",$time);
-    fork
-      driver_inst.run();
-      checker_inst.run();
-      scoreboard_inst.run();
-      agent_inst.run();
-    join_none
-  endtask
+    task run;
+        $display("[%g] El Agente fue inicializado", $time);
+        forever begin
+            #1
+            if(test_agent_mbx.num() > 0) begin
+                $display("[%g] Agente: se recibe instruccion", $time);
+                test_agent_mbx.get(instruccion);
+                case(instruccion)
+                    llenado_aleatorio: begin // Esta instruccion genera num_tranacciones escrituras seguidas del mismo número de lecturas
+                        for(int i = 0; i < num_transacciones; i++) begin
+                            transaccion = new;
+                            transaccion.max_retardo = max_retardo;
+                            transaccion.randomize();
+                            tpo_spec = escritura;
+                            transaccion.tipo = tpo_spec;
+                            transaccion.print("Agente: transacción creada");
+                            agnt_drv_mbx.put(transaccion);
+                        end
+                        for(int i = 0; i < num_transacciones; i++) begin
+                            transaccion = new;
+                            transaccion.randomize();
+                            tpo_spec = lectura;
+                            transaccion.tipo = tpo_spec;
+                            transaccion.print("Agente: transacción creada");
+                            agnt_drv_mbx.put(transaccion);
+                        end
+                    end
+                    trans_aleatoria: begin // Esta instruccion genera transacciones aleatorias
+                        transaccion = new;
+                        transaccion.max_retardo = max_retardo;
+                        transaccion.randomize();
+                        transaccion.print("Agente: transacción creada");
+                        agnt_drv_mbx.put(transaccion);
+                    end
+                    trans_especifica: begin
+                        transaccion = new;
+                        transaccion.tipo = tpo_spec;
+                        transaccion.dato = dto_spec;
+                        transaccion.retardo = ret_spec;
+                        transaccion.print("Agente: transacción creada");
+                        agnt_drv_mbx.put(transaccion);
+                    end
+                    sec_trans_aleatorias: begin 
+                        for (int i = 0; i < num_transacciones; i++) begin
+                            transaccion = new;
+                            transaccion.max_retardo = max_retardo;
+                            transaccion.randomize();
+                            transaccion.print("Agente: transacción creada");
+                            agnt_drv_mbx.put(transaccion);
+                        end
+                    end
+                endcase
+            end
+        end
+    endtask
 endclass
