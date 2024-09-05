0984541410e29ebdef71e5cb17b9ded180ebbe89
pedroxdgpg <pedro.medinila@estudiantec.cr>
2024-09-05 00:21:57 -0600

    Prueba un solo flanco driver

--- driver.sv
+++ driver.sv
@@ -59,7 +59,7 @@ class driver #(parameter width = 16);
                     vif.push = 0; // Desactiva la escritura
 
                     transaction.dato = vif.dato_out;
-                    @(negedge vif.clk);
+                    //@(negedge vif.clk);
                     vif.pop = 1;
 
                     drv_chkr_mbx.put(transaction);
